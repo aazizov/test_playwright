0d9c68df66e429b7543bfc35645315715e086662
“Alakbar <a.azizov@yahoo.com>
2025-01-27 17:31:28 +0400

    First Commit

---
+++ src/test/java/com/example/test_playwright/Constants.java
@@ -0,0 +1,18 @@
+package com.example.test_playwright;
+
+public class Constants {
+    public static final String CHROMIUM_PATH = "D:\\Bin\\chrome-win\\chrome.exe";
+    public static final Boolean INVISIBILITY = true;
+    public static final Integer DELAY_MS = 0;
+    public static final String LOGIN_PATH = "http://10.13.44.133:8081/";
+
+    public static final String LOGIN = "chingiztest";
+    public static final String PASSWORD = "#Aa12345%";
+
+    public static final String EXCEL_FILE_PATH = "";
+    public static final Boolean WRITE_VIDEO = false;
+    public static final Boolean CREATE_TRACE = false;
+    public static final Boolean ANALYZE_SYSTEM_LOG = false;
+
+    public static final String PROVIDER_NAME = "BBTV";
+}
